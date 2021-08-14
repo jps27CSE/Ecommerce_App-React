a19e00a4fea41a506d59966339254abc3500e5b8
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-08-15 01:00:18 +0600

    Shop Page Implement

--- src/components/collection-preview/collection-preview.js
+++ src/components/collection-preview/collection-preview.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import './collection-preview.scss'
 
-const CollectionPreview =({title,items})=>(
+const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.map(item=>(
+            {items.filter((item, idx) => idx < 4).map(item => (
                 <div key={item.id}>{item.name}</div>
             ))}
         </div>
